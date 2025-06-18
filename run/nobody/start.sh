c8a0d994045e6440dd68e4757978346049a10f3f
binhex <megalith01@gmail.com>
2025-06-18 14:05:27 +0100

    fix for new env var name

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/dumb-init /bin/bash
 
 # use bash script to set incoming port and bind adapter and then start qBittorrent
-/usr/local/bin/portget.sh /usr/bin/qbittorrent-nox --webui-port="${WEBUI_PORT}" --profile=/config
+/usr/local/bin/portget.sh /usr/bin/qbittorrent-nox --webui-port="${APPLICATION_PORT}" --profile=/config
