9fbf839db08d293d1a4a31bc5af4c834adfb5817
binhex <megalith01@gmail.com>
2025-06-28 17:30:35 +0100

    rework start.sh

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,4 +1,36 @@
 #!/usr/bin/dumb-init /bin/bash
 
-# use bash script to set incoming port and bind adapter and then start qBittorrent
-/usr/local/bin/portget.sh /usr/bin/qbittorrent-nox --webui-port="${APPLICATION_PORT}" --profile=/config
+function start_qbittorrent() {
+
+	echo "[INFO] Starting ${APPLICATION_NAME} Web UI..."
+
+	# run process non daemonised (blocking)
+	/usr/bin/qbittorrent-nox --webui-port="${APPLICATION_PORT}" --profile=/config
+
+}
+
+function common() {
+
+  local session_lock_filepath="/config/qBittorrent/data/BT_backup/session.lock"
+
+  echo "[info] Removing ${APPLICATION_NAME} session lock file (if it exists)..."
+  rm -f "${session_lock_filepath}"
+
+}
+
+function main() {
+
+	# running common setup tasks
+	common
+
+	if [[ "${CONFIGURE_INCOMING_PORT}" == "yes" ]]; then
+
+		echo "[info] Starting ${APPLICATION_NAME} Web UI with port configuration..."
+		/usr/local/bin/portget.sh --application-name "${APPLICATION_NAME}" --application-port "${APPLICATION_PORT}" /usr/bin/qbittorrent-nox --webui-port="${APPLICATION_PORT}" --profile=/config
+	else
+		echo "[info] Skipping port configuration as env var 'CONFIGURE_INCOMING_PORT' is not set to 'yes'"
+		start_qbittorrent
+	fi
+}
+
+main
