1435f3603af8f807860028ad0273c192f6030412
binhex <megalith01@gmail.com>
2025-06-28 20:14:29 +0100

    switch env var name

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -23,12 +23,12 @@ function main() {
 	# running common setup tasks
 	common
 
-	if [[ "${CONFIGURE_INCOMING_PORT}" == "yes" ]]; then
+	if [[ "${GLUETUN_INCOMING_PORT}" == "yes" ]]; then
 
 		echo "[info] Starting qbittorrent Web UI with port configuration..."
 		/usr/local/bin/portget.sh --application-name 'qbittorrent' --application-port "${APPLICATION_PORT}" /usr/bin/qbittorrent-nox --webui-port="${APPLICATION_PORT}" --profile=/config
 	else
-		echo "[info] Skipping port configuration as env var 'CONFIGURE_INCOMING_PORT' is not set to 'yes'"
+		echo "[info] Skipping VPN incoming port configuration as env var 'GLUETUN_INCOMING_PORT' is not set to 'yes'"
 		start_qbittorrent
 	fi
 }
