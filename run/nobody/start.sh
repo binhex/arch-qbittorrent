d99cbed0e265458fde1cd5ad70f628315c9ea781
binhex <megalith01@gmail.com>
2025-07-03 12:53:37 +0100

    switch param name

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -29,11 +29,11 @@ function main() {
 	else
 		echo "[info] Using WEBUI_PORT=${WEBUI_PORT}"
 	fi
-	
+
 	if [[ "${GLUETUN_INCOMING_PORT}" == "yes" ]]; then
 
 		echo "[info] Starting qbittorrent Web UI with port configuration..."
-		/usr/local/bin/portget.sh --application-name 'qbittorrent' --application-port "${WEBUI_PORT}" /usr/bin/qbittorrent-nox --webui-port="${WEBUI_PORT}" --profile=/config
+		/usr/local/bin/portget.sh --application-name 'qbittorrent' --webui-port "${WEBUI_PORT}" /usr/bin/qbittorrent-nox --webui-port="${WEBUI_PORT}" --profile=/config
 	else
 		echo "[info] Skipping VPN incoming port configuration as env var 'GLUETUN_INCOMING_PORT' is not set to 'yes'"
 		start_qbittorrent
